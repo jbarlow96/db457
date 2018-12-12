0f3c06b858e86e111ceda27413a9493af0c31b73
jbarlow96 <33383985+jbarlow96@users.noreply.github.com>
2018-12-12 13:53:54 -0500

    added artist functions in book reservation

--- src/pages/BookOnline.jsx
+++ src/pages/BookOnline.jsx
@@ -12,9 +12,17 @@ export default class BookOnline extends Component {
       artist: '',
       date: '',
       Res_id: '',//just added
+      Fname: '',
+      Lname: '',
+      Shop_id: '',
+      Artist_rating: '',
+      Artist_id: '',
       responseSuccess: null,
       responseDeleted:null,
-      responseUpdated:null
+      responseUpdated:null,
+      responseArtSuccess: null,
+      responseArtDeleted: null,
+      responseArtUpdated: null
      
     };
   }
@@ -108,8 +116,77 @@ export default class BookOnline extends Component {
       this.setState({ responseDeleted: data.success });
     })
     .catch(err => console.log('ERROR: ', err));
-};
-    
+  };
+  
+  // Add artist
+  requestArtInfo = e => {
+    e.preventDefault();
+    const artistBody = {
+      Artist_id: this.state.Artist_id,
+      Fname: this.state.Fname,
+      Lname: this.state.Lname,
+      Shop_id: this.state.Shop_id,
+      Artist_rating: this.state.Artist_rating
+    };
+    fetch('http://localhost:4000/addartist', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(artistBody)
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('RESPONSE: ', data);
+        this.setState({ responseArtSuccess: data.success });
+      })
+      .catch(err => console.log('ERROR: ', err));
+    };
+
+    // Artist Update
+    requestArtUpdate = e => {
+      e.preventDefault();
+      const artistBody = {
+        Artist_id: this.state.Artist_id,
+        Fname: this.state.Fname,
+        Lname: this.state.Lname,
+        Shop_id: this.state.Shop_id,
+        Artist_rating: this.state.Artist_rating
+      };
+      fetch('http://localhost:4000/updateartist/'+this.state.Artist_id, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(artistBody)
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('RESPONSE: ', data);
+          this.setState({ responseArtUpdated: data.success });
+        })
+        .catch(err => console.log('ERROR: ', err));
+      };
+
+      // Delete Artist
+      requestArtDelete = e =>{
+        e.preventDefault();
+      console.log('http://localhost:4000/deleteartist/'+this.state.Artist_id);
+      fetch('http://localhost:4000/deleteartist/'+this.state.Artist_id, {
+        method: 'GET',
+       
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: undefined
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('RESPONSE: ', data);
+          this.setState({ responseArtDeleted: data.success });
+        })
+        .catch(err => console.log('ERROR: ', err));
+      };
   
       //
   render() {
@@ -331,7 +408,6 @@ export default class BookOnline extends Component {
           <div className="row">
             <div className="col-md-6">
               <form onSubmit={this.requestDelete}>
-               
               <div className="form-group">
                   <label htmlFor="Artist">Reservation ID</label>
                   <input
@@ -354,7 +430,201 @@ export default class BookOnline extends Component {
             </div>
           </div>
         </section>
+
+
+
+        <section>
+          <div className="page-header">
+            <h2>Create Artist </h2>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <form onSubmit={this.requestArtInfo}>
+              <div className="form-group">
+                  <input
+                    type="number"
+                    name="Artist_id"
+                    id="Artist_id"
+                    placeholder="Enter #"
+                    maxLength="12"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="Fname"
+                    id="Fname"
+                    placeholder="First Name"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="Lname"
+                    id="Lname"
+                    placeholder="Last Name"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Shop_id">Shop_id</label>
+                  <select
+                    className="form-control input-lg"
+                    name="Shop_id"
+                    id="Shop_id"
+                    required
+                    onChange={this.handleChange}
+                  >
+                    <option value="">Please Select...</option>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="number"
+                    name="Artist_rating"
+                    id="Artist_rating"
+                    placeholder="Rating #"
+                    maxLength="12"
+                    min = "0"
+                    max = "5"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <button type="submit" className="btn button btn-lg">
+                  Create Artist
+                </button>
+                <h1>{this.state.responseArtSuccess}</h1>
+              </form>
+            </div>
+          </div>
+        </section>
+
+
+        <section>
+          <div className="page-header">
+            <h2>Update Artist </h2>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <form onSubmit={this.requestArtUpdate}>
+              <div className="form-group">
+                  <input
+                    type="number"
+                    name="Artist_id"
+                    id="Artist_id"
+                    placeholder="Enter #"
+                    maxLength="12"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="Fname"
+                    id="Fname"
+                    placeholder="First Name"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    name="Lname"
+                    id="Lname"
+                    placeholder="Last Name"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="Shop_id">Shop_id</label>
+                  <select
+                    className="form-control input-lg"
+                    name="Shop_id"
+                    id="Shop_id"
+                    required
+                    onChange={this.handleChange}
+                  >
+                    <option value="">Please Select...</option>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                  </select>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="number"
+                    name="Artist_rating"
+                    id="Artist_rating"
+                    placeholder="Rating #"
+                    maxLength="12"
+                    min = "0"
+                    max = "5"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <button type="submit" className="btn button btn-lg">
+                  Update Artist
+                </button>
+                <h1>{this.state.responseArtUpdated}</h1>
+              </form>
+            </div>
+          </div>
+        </section>
+
+
+        <section>
+          <div className="page-header">
+            <h2>Delete Artist </h2>
+          </div>
+          <div className="row">
+            <div className="col-md-6">
+              <form onSubmit={this.requestArtDelete}>
+              <div className="form-group">
+                  <label htmlFor="Artist">ARTIST ID</label>
+                  <input
+                    type="number"
+                    name="Artist_id"
+                    id="Artist_id"
+                    placeholder="Artist ID"
+                    className="form-control input-lg"
+                    required
+                    onChange={this.handleChange}
+                  />
+                </div>
+        
+          
+                <button type="submit" className="btn button btn-lg">
+                  Delete Artist
+                </button>
+                <h1>{this.state.responseArtDeleted}</h1>
+              </form>
+            </div>
+          </div>
+        </section>
+
       </main>
     );
-  }
+    }
 }
+
